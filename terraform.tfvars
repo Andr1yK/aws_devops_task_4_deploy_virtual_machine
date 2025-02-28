6b2a28f7ce6a68ee1ec42ad8a2a41e9dfd5ff6a8
Andriy Kobetiak <andriy.k@mate.academy>
2025-02-28 23:45:49 +0200

    solution

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
-subnet_id = ""
-security_group_id = ""
+subnet_id         = "subnet-0bda938f684cebd13"
+security_group_id = "sg-086814e7b4a0fbc05"
+path_to_public_key = "~/.ssh/id_rsa.pub"
--- variables.tf
+++ variables.tf
@@ -1,9 +1,14 @@
 variable "subnet_id" {
-    type = string
-    description = "ID of the VPC subnet, you deployed in the previous task."
+  type        = string
+  description = "ID of the VPC subnet, you deployed in the previous task."
 }
 
 variable "security_group_id" {
-    type = string
-    description = "ID of the security group, you deployed in the previous task."
+  type        = string
+  description = "ID of the security group, you deployed in the previous task."
+}
+
+variable "path_to_public_key" {
+  type        = string
+  description = "Path to the public key file."
 }
